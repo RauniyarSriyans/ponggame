69a49d5de819c82d016deb9f259c02da54a4ac0f
Sriyans Rauniyar <rauniyar@princeton.edu>
2022-03-18 06:14:22 -0400

    final

---
+++ ball.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, width
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
+    
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+    
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x_l_paddle(self):
+        self.x_move = (abs(self.x_move))
+        self.move_speed *= 0.9
+ 
+    def bounce_x_r_paddle(self):
+        self.x_move = -(abs(self.x_move))
+        self.move_speed *= 0.9
+    
+    def reset_position(self):
+        self.goto(0,0) 
+        self.move_speed = 0.1
+        self.bounce_x()
+
